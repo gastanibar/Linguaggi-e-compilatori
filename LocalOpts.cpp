fbcb26ebc0bc25fa05cbeeffdc225225600f6ff4
gastanibar <tolveandrea943@gmail.com>
2024-04-03 19:29:35 +0200

    Ho aggiunto l'ottimizzazione per la divisione e l'algebraic identity

--- LocalOpts.cpp
+++ LocalOpts.cpp
@@ -9,33 +9,64 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/IRBuilder.h"
 // L'include seguente va in LocalOpts.h
 #include <llvm/IR/Constants.h>
 using namespace llvm;
 
-bool runOnBasicBlock(BasicBlock &B) {
-    for(auto &I : B){ 
-    	if(I.getNextNode()!=nullptr){
-	    Instruction &Inst1st = I, &Inst2nd = *(I.getNextNode());
-	    int i=1; //tengo conto della posizione del registro virtuale
-	    if(Inst1st.getOpcode() == Instruction::Mul){
-	        for(auto *Iter = Inst1st.op_begin(); Iter != Inst1st.op_end(); ++Iter){
-		    Value *Op = *Iter;
-		    if(ConstantInt *C = dyn_cast<ConstantInt>(Op)){
-			    if(C->getValue().isPowerOf2()){
-				    auto val = C->getValue().exactLogBase2();
-				    APInt app(32, val);
-				    ConstantInt *CC = ConstantInt::get(Inst1st.getOperand(i)->getType()->getContext(), app);
-				    Instruction *ShiftInst = BinaryOperator::Create(Instruction::Shl, Inst1st.getOperand(i), CC);
-				    ShiftInst->insertAfter(&Inst1st);
-				    Inst1st.replaceAllUsesWith(ShiftInst);
-	            	    }
-		     }
-		    i--;
-	          }
-
-             }
+void strengthReduction(Instruction &Inst1st, bool mul){
+  int i=1;
+  Instruction *ShiftInst;
+  for(auto *Iter = Inst1st.op_begin(); Iter != Inst1st.op_end(); ++Iter){
+        Value *Op = *Iter;
+        if(ConstantInt *C = dyn_cast<ConstantInt>(Op)){
+            if(C->getValue().isPowerOf2()){
+              auto val = C->getValue().exactLogBase2();
+              APInt app(32, val);
+              ConstantInt *CC = ConstantInt::get(Inst1st.getOperand(i)->getType()->getContext(), app);
+              if(mul)
+                ShiftInst = BinaryOperator::Create(Instruction::Shl, Inst1st.getOperand(i), CC);
+              else 
+                ShiftInst = BinaryOperator::Create(Instruction::AShr, Inst1st.getOperand(i), CC);
+              ShiftInst->insertAfter(&Inst1st);
+              Inst1st.replaceAllUsesWith(ShiftInst);
+            }
         }
+        i--;
+  }
+}
+
+bool algebraicIdentity(Instruction &Inst1st, bool add){
+  int i=1;
+  for(auto *Iter = Inst1st.op_begin(); Iter != Inst1st.op_end(); ++Iter){
+      Value *Op = *Iter;
+      if(ConstantInt *C = dyn_cast<ConstantInt>(Op)){
+        if((C->getValue() == 0 && add) || (C->getValue() == 1 && !add)){
+            Instruction &Inst2st = *(Inst1st.getNextNode());
+            AllocaInst *ptr = new AllocaInst(Inst1st.getOperand(i)->getType(),0, "", &Inst2st);
+            StoreInst *storeInst = new StoreInst(Inst1st.getOperand(i), ptr, &Inst2st);
+            LoadInst *loadInst = new LoadInst(Inst1st.getOperand(i)->getType(), ptr, "", &Inst2st);
+            Inst1st.replaceAllUsesWith(loadInst);
+            return true;
+        }
+      }
+      i--;
+  }
+  return false;
+}
+
+bool runOnBasicBlock(BasicBlock &B) {
+    for(auto &Inst1st : B){ 
+        //prima di tutto cerco di ottimizzare una Algebraic Identity
+        if(Inst1st.getOpcode() == Instruction::Add)
+          algebraicIdentity(Inst1st, true);
+        else if(Inst1st.getOpcode() == Instruction::Mul){
+          if(!algebraicIdentity(Inst1st, false)) //se eseguo la algebraic identity non faccio la strength reduction
+              strengthReduction(Inst1st, true);
+        }
+        else if(Inst1st.getOpcode() == Instruction::SDiv)
+          strengthReduction(Inst1st, false);
+        
     }
     return true;
 }
